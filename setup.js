f7b938b116f33b1cade30a27e2a060897c2de513
jkdellow <jennifer.dellow@gmail.com>
2019-10-03 12:42:16 -0400

    Moving aws credentials into Dynamo setup
    This stops my env variables from being ignored, in favour of these static ones, for other services.

--- setup.js
+++ setup.js
@@ -3,10 +3,6 @@ const cwd = require('cwd');
 const DynamoDB = require('aws-sdk/clients/dynamodb');
 const DynamoDbLocal = require('dynamodb-local');
 
-// aws-sdk requires access and secret key to be able to call DDB
-process.env.AWS_ACCESS_KEY_ID = 'access-key';
-process.env.AWS_SECRET_ACCESS_KEY = 'secret-key';
-
 const DEFAULT_PORT = 8000;
 
 module.exports = async function() {
@@ -16,7 +12,9 @@ module.exports = async function() {
   const dynamoDB = new DynamoDB({
     endpoint: 'localhost:' + port,
     sslEnabled: false,
-    region: 'local-env'
+    region: 'local-env',
+    accessKeyId: 'access-key',
+    secretAccessKey: 'secret-key'
   });
   global.__DYNAMODB__ = await DynamoDbLocal.launch(port, null, ['-sharedDb']);
 
